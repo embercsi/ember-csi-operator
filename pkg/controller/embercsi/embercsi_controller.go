ca3c29b66203107ae0cd1035b0befc5fd14918f3
Kiran Thyagaraja <kthyagaraja@gmail.com>
2019-01-25 13:47:35 -0600

    Check for LVM volume driver and mount lvm-specific directories.

--- pkg/controller/embercsi/embercsi_controller.go
+++ pkg/controller/embercsi/embercsi_controller.go
@@ -530,12 +530,12 @@ func getVolumeMounts(ecsi *embercsiv1alpha1.EmberCSI, csiDriverMode string) []co
 	}
 
         // Check to see if the volume driver is LVM
-        if len(ecsi.Spec.Config.EnvVars.X_CSI_BACKEND_CONFIG) > 0 && strings.Contains(ecsi.Spec.Config.EnvVars.X_CSI_BACKEND_CONFIG, "cinder.volume.drivers.lvm.LVMVolumeDriver")  {
+        if len(ecsi.Spec.Config.EnvVars.X_CSI_BACKEND_CONFIG) > 0 && strings.Contains(strings.ToLower(ecsi.Spec.Config.EnvVars.X_CSI_BACKEND_CONFIG), "lvmvolume")  {
 		vm = append(vm, corev1.VolumeMount{
 			Name: "etc-lvm",
 			MountPath: "/etc/lvm",
 			MountPropagation: &bidirectional,
-		},{
+		}, corev1.VolumeMount{
 			Name: "var-lock-lvm",
 			MountPath: "/var/lock/lvm",
 			MountPropagation: &bidirectional,
@@ -650,7 +650,7 @@ func getVolumes (ecsi *embercsiv1alpha1.EmberCSI, csiDriverMode string) []corev1
 	}
 
         // Check to see if the volume driver is LVM
-        if len(ecsi.Spec.Config.EnvVars.X_CSI_BACKEND_CONFIG) > 0 && strings.Contains(ecsi.Spec.Config.EnvVars.X_CSI_BACKEND_CONFIG, "cinder.volume.drivers.lvm.LVMVolumeDriver")  {
+        if len(ecsi.Spec.Config.EnvVars.X_CSI_BACKEND_CONFIG) > 0 && strings.Contains(strings.ToLower(ecsi.Spec.Config.EnvVars.X_CSI_BACKEND_CONFIG), "lvmvolume")  {
 		vol = append(vol, corev1.Volume{
 			Name: "etc-lvm",
 			VolumeSource: corev1.VolumeSource{
@@ -658,7 +658,7 @@ func getVolumes (ecsi *embercsiv1alpha1.EmberCSI, csiDriverMode string) []corev1
 					Path: "/etc/lvm",
 				},
 			},
-		},{
+		}, corev1.Volume{
 			Name: "var-lock-lvm",
 			VolumeSource: corev1.VolumeSource{
 				HostPath: &corev1.HostPathVolumeSource{
