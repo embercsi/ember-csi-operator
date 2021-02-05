769c0d730b252922b47cca353bde30df68a3ce12
Gorka Eguileor <geguileo@redhat.com>
2021-02-05 13:44:08 +0100

    Use host tools
    Ember-CSI stoped using iSCSI and multipath tools inside the container
    and started using the host's, which fixes incompatibility issues.
    
    This patch changes the way we deploy to take that into account.
    
    We no longer need most iSCSI and multipath mounts, but the container
    needs to be able to access the host's IPC.
    
    OS-Brick accesses /etc/iscsi/initiatorname.iscsi so we still mount
    /etc/iscsi.
    
    The patch also updates the crclvm.sh example script to work around some
    nuisances of CRC: iscsid usually fails to start on VM boot,
    multipath.conf doesn't exist, etc.

--- deploy/examples/crclvm.sh
+++ deploy/examples/crclvm.sh
@@ -5,6 +5,13 @@ CRC=${1:-crc}
 SECRET=${2:-pull-secret}
 SOURCE=${3:-community-operators}
 
+
+function do_ssh {
+  SSH_PARAMS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~`whoami`/.crc/machines/crc/id_rsa"
+  SSH_REMOTE="core@`${CRC} ip`"
+  ssh $SSH_PARAMS $SSH_REMOTE "$@"
+}
+
 # Setup CRC env
 [ ! -e ${SECRET} ] && echo '{"auths":{"fake":{"auth": "bar"}}}' > ${SECRET}
 ${CRC} delete -f || true
@@ -18,8 +25,17 @@ $(${CRC} console --credentials | grep -o "oc login -u kubeadmin.*443")
 ID=`oc get clusterversion version -ojsonpath='{range .spec.overrides[*]}{.name}{"\n"}{end}' | nl -v 0 -w 1 | grep csi-snapshot-controller-operator | cut -f 1`
 oc patch clusterversion/version --type='json' -p '[{"op":"remove", "path":"/spec/overrides/'${ID}'"}]'
 
-# Deploy LVM container in the background
-sed -e "s/latest/7/g" deploy/examples/lvmbackend.yaml | oc create -f -
+
+# Create LVM VG if it doesn't exist
+do_ssh 'sudo bash -c '\''if ! vgdisplay ember-volumes ; then truncate -s 10G /var/lib/containers/ember-volumes && device=`losetup --show -f /var/lib/containers/ember-volumes ` && echo -e \"device is $device\n\" && pvcreate $device && vgcreate ember-volumes $device && vgscan && sed -i "s/^\tudev_sync = 1/\tudev_sync = 0/" /etc/lvm/lvm.conf && sed -i "s/^\tudev_rules = 1/\tudev_rules = 0/" /etc/lvm/lvm.conf; fi'\'
+
+# Ensure iscsid is running on the host, because with CRC sometimes it fails to start on VM start on CRC 1.17
+# On CRC 1.20 it fails to start because it doesn't have the initiatorname in the system
+do_ssh 'if [[ ! -e /etc/iscsi/initiatorname.iscsi ]]; then echo InitiatorName=`iscsi-iname` | sudo tee /etc/iscsi/initiatorname.iscsi; fi'
+do_ssh 'sudo systemctl restart iscsid'
+
+# Multipath doesn't have a configuration, so we need to create it
+do_ssh 'if [[ ! -e /etc/multipath.conf ]]; then sudo mpathconf --enable --with_multipathd y --user_friendly_names n --find_multipaths y && sudo systemctl start multipathd; fi'
 
 # Setup custom marketplace to install devel branch of ember operator
 if [ "$SOURCE" != "community-operators" ]; then
