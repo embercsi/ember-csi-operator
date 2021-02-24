554596726f8c2d681ccab0fdb27fa0d1b3c6261d
Christian Schwede <cschwede@redhat.com>
2021-02-24 09:09:36 +0100

    Update fieldDependency tag
    This has been a leftover that was already fixed in the
    community-operator repo, let's fix the template as well.

--- build/olm-catalog/yaml-options-gen.py
+++ build/olm-catalog/yaml-options-gen.py
@@ -412,7 +412,7 @@ def render_option(option, driver, group_options=False):
           x-descriptors:
             - 'urn:alm:descriptor:com.tectonic.ui:fieldGroup:{group}'
             - {opt_type}
-            - 'urn:alm:descriptor:com.tectonic.ui:fieldDependency:spec.config.envVars.X_CSI_BACKEND_CONFIG.driver:{driver}'""".format(
+            - 'urn:alm:descriptor:com.tectonic.ui:fieldDependency:config.envVars.X_CSI_BACKEND_CONFIG.driver:{driver}'\n""".format(
                name=option.option_name(driver),  # noqa
                display_name=option.display_name,
                description='"' + option.help + '"',
